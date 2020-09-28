7e4e6377cc3ed8a33bb9e345a4ed073e69e635d7
annacia <carol.inhasz@hotmail.com>
2020-09-28 19:01:36 -0300

    code for nonzero guessed words

--- src/GuessedWords.test.js
+++ src/GuessedWords.test.js
@@ -27,12 +27,31 @@ describe('if there are no words guessed', () => {
         expect(component.length).toBe(1);
     })
     test('renders instructions to a guess word', () => {
-        console.log(wrapper)
         const instructions = findByTestAttr(wrapper, 'guess-instructions');
         expect(instructions.text().length).not.toBe(0);
     })
 });
 
 describe('if there are words guessed', () => {
-
+    const guessedWords = [
+        {guessedWords: 'train', letterMatchCount: 3},
+        {guessedWords: 'agile', letterMatchCount: 1},
+        {guessedWords: 'party', letterMatchCount: 5},
+    ]
+    let wrapper;
+    beforeEach(() => {
+        wrapper = setup({ guessedWords })
+    })
+    test('renders without error', () => {
+        const component = findByTestAttr(wrapper, 'component-guessed-words');
+        expect(component.length).toBe(1);
+    });
+    test('renders guessed words section', () => {
+        const guessedWordsNode = findByTestAttr(wrapper, 'guessed-words');
+        expect(guessedWordsNode.length).toBe(1);
+    });
+    test('display correct number of guessed words', () => {
+        const guessedWordsNode = findByTestAttr(wrapper, 'guessed-word');
+        expect(guessedWordsNode.length).toBe(guessedWords.length);
+    });
 });
