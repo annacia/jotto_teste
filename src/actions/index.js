fdb32cc64006a437a464ffab85b42d8bbd31c9ef
aaureliano <aaureliano@senhasegura.com.br>
2021-01-05 09:21:28 -0300

    setup guessWord test

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,22 @@
 export const actionTypes = {
-    CORRECT_GUESS: 'CORRECT_GUESS'
+    CORRECT_GUESS: 'CORRECT_GUESS',
+    GUESS_WORD: 'GUESS_WORD'
 };
 
-export function correctGuess() {
-    return { type: actionTypes.CORRECT_GUESS};
-}
+export const guessWord = (guessedWord) => {
+    return function(dispatch, getState) {
+
+    };
+};
+
+//Tests removed
+// import { correctGuess, actionTypes } from "./index";
+//
+// describe('correctGuess', () => {
+//     test('returns an action with type `CORRECT_GUESS`', () => {
+//         const action = correctGuess();
+//         //expect(action).toBe({});
+//         //toBe can ONLY be used for IMMUTABLE objects like numbers and strings
+//         expect(action).toEqual({ type: actionTypes.CORRECT_GUESS });
+//     })
+// })
