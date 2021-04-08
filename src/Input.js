868df2348e4b2063a7b2e5c66f002043122a7ee6
aaureliano <aaureliano@senhasegura.com.br>
2021-04-08 13:09:43 -0300

    make testes and code for gueeWord arg

--- src/Input.js
+++ src/Input.js
@@ -20,6 +20,7 @@ export class UnconnectedInput extends Component {
 
         if (guessedWord && guessedWord.length > 0) {
             this.props.guessWord(guessedWord);
+            this.setState({currentGuess: ''});
         }
     }
 
--- src/Input.test.js
+++ src/Input.test.js
@@ -98,5 +98,8 @@ describe('guessWord action creator call', () => {
     test("calls guessWord with input value as argument", () => {
         const guessedWordArg = guessWordMock.mock.calls[0][0];
         expect(guessedWordArg).toBe(guessedWord);
-    })
+    });
+    test("input box clears on submit", () => {
+       expect(wrapper.state('currentGuess')).toBe('');
+    });
 });
