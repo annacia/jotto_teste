1ab71d7b397c6436f2e9a01106d036effff9001e
aaureliano <aaureliano@senhasegura.com.br>
2021-04-08 12:24:50 -0300

    make testes and code for gueeWord arg

--- src/Input.js
+++ src/Input.js
@@ -4,6 +4,25 @@ import {connect} from 'react-redux';
 import { guessWord } from "./actions";
 
 export class UnconnectedInput extends Component {
+    constructor(props) {
+        super(props);
+
+        //initialize state
+        this.state = {currentGuess: null}
+
+        //bind this for submitGuessedWord
+        this.submitGuessedWord = this.submitGuessedWord.bind(this);
+    }
+
+    submitGuessedWord(evt) {
+        evt.preventDefault();
+        const guessedWord = this.state.currentGuess;
+
+        if (guessedWord && guessedWord.length > 0) {
+            this.props.guessWord(guessedWord);
+        }
+    }
+
     render() {
         const content = this.props.success
         ? null
@@ -13,12 +32,14 @@ export class UnconnectedInput extends Component {
                     data-test="input-box"
                     className="mb-2 mx-sm-3"
                     type="text"
+                    value={this.state.currentGuess}
+                    onChange={(evt) => this.setState({currentGuess: evt.target.value})}
                     placeholder="enter guess"
                 />
                 <button
                     data-test="submit-button"
                     className="btn btn-primary mb-2"
-                    onClick={() => this.props.guessWord('train')}
+                    onClick={(evt) => this.submitGuessedWord(evt)}
                     type="submit"
                 >
                     Submit
