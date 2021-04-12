870f4eb2aad9b77d34adb266cbe0032633a80668
aaureliano <aaureliano@senhasegura.com.br>
2021-04-12 16:56:53 -0300

    basis for app component

--- src/App.js
+++ src/App.js
@@ -1,35 +1,54 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
+import React from "react";
 import './App.css';
 
-import GuessedWords from "./GuessedWords";
 import Congrats from "./Congrats";
+import GuessedWords from "./GuessedWords";
 import Input from "./Input";
-import {getSecretWord} from "./actions";
 
-export class UnconnectedApp extends Component {
-    componentDidMount() {
-        //get the secret word
-        this.props.getSecretWord();
-    }
-
-
-
-    render() {
-    return(
-        <div className="container">
-          <h1>Jotto</h1>
-          {/*<Congrats success={this.props.success}/>*/}
-          {/*<Input/>*/}
-          {/*<GuessedWords guessedWords={this.props.guessedWords}/>*/}
+function App() {
+    return (
+        <div data-test="component-app" className="container">
+            <h1>Jotto</h1>
+            <Congrats success={true} />
+            <GuessedWords guessedWords={[{guessedWord: 'train', letterMatchCount: 3}]} />
         </div>
-    )
-  }
+    );
 }
 
-const mapStateToProps = (state) => {
-    const {success, guessedWords, secretWord} = state;
-    return {success, guessedWords, secretWord}
-}
+export default App;
 
-export default connect(mapStateToProps, {getSecretWord})(UnconnectedApp);
+// import React, {Component} from 'react';
+// import {connect} from 'react-redux';
+// import './App.css';
+//
+// import GuessedWords from "./GuessedWords";
+// import Congrats from "./Congrats";
+// import Input from "./Input";
+// import {getSecretWord} from "./actions";
+//
+// export class UnconnectedApp extends Component {
+//     componentDidMount() {
+//         //get the secret word
+//         this.props.getSecretWord();
+//     }
+//
+//
+//
+//     render() {
+//     return(
+//         <div data-test="component-app" className="container">
+//           <h1>Jotto</h1>
+//           {/*<Congrats success={this.props.success}/>*/}
+//           {/*<Input/>*/}
+//           {/*<GuessedWords guessedWords={this.props.guessedWords}/>*/}
+//         </div>
+//     )
+//   }
+// }
+//
+// const mapStateToProps = (state) => {
+//     const {success, guessedWords, secretWord} = state;
+//     return {success, guessedWords, secretWord}
+// }
+//
+// export default connect(mapStateToProps, {getSecretWord})(UnconnectedApp);
