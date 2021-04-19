340685ec6032f6ff7f220e68966ad5b8f53c20bf
aaureliano <aaureliano@senhasegura.com.br>
2021-04-19 11:34:54 -0300

    test and code loading spinner

--- src/App.js
+++ src/App.js
@@ -16,11 +16,9 @@ const reducer = (state, action) => {
 }
 
 function App() {
-    // const [secretWord, setSecretWord] = useState('');
-
     const [state, dispatch] = React.useReducer(
         reducer,
-        {secretWord: ''}
+        {secretWord: null}
     );
 
   // TODO: get props from shared state
@@ -34,6 +32,17 @@ function App() {
   useEffect(() => {
     getSecretWord(setSecretWord);
   }, [])
+
+    if (state.secretWord === null) {
+        return (
+            <div className="container" data-test="spinner">
+                <div className="spinner-border" role="status">
+                    <span className="sr-only">Loading...</span>
+                </div>
+                <p>Loading secret word...</p>
+            </div>
+        )
+    }
 
   return (
     <div data-test="component-app" className="container">
